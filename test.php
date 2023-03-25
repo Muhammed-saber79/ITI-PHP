2e3994020c8f7851178b59e513c140bf5d603077
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-03-25 17:22:53 +0200

    Day 1 Solution

---
+++ test.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <!-- Start Card -->
+    <div class="card text-start">
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">
+            Thanks <?php if($_POST['gender']=='male'){echo "Mr";}else{echo "Mrs";} echo " ".$_POST['firstName']." ".$_POST['lastName']; ?>
+        </p>
+        <h4>Please Review Your Information:</h4>
+        Name: <?php echo $_POST['firstName']." ".$_POST['lastName'] ?><br>
+        Address: <?php echo $_POST['address'] ?><br>
+        Skills: <?php foreach($_POST['skills'] as $value){
+            echo $value."-";
+        } ?> <br>
+        Department: <?php echo $_POST['department'] ?>
+      </div>
+    </div>
+    <!-- End Card -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
